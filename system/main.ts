db6d2b64d95d313a4dd5f59e47ec3bb5b8c280ce
Nikhil Saraf <nsaraf@purestorage.com>
2022-02-18 00:08:14 -0800

    update shit

--- system/main.ts
+++ system/main.ts
@@ -1,21 +1,161 @@
-import {
-  Server,
-  listenAndServe,
-} from "https://deno.land/std@0.126.0/http/server.ts";
-import { GraphQLHTTP } from "https://deno.land/x/gql@1.1.1/mod.ts";
+import { createClient } from "./deps.js";
+// Get the connection string from the environment variable "DATABASE_URL"
+// const databaseUrl = Deno.env.get("DATABASE_URL")!;
+// const pool = new postgres.Pool(`postgres://postgres:8f187cbf0264f6d480127cfe36bc0828f966a157637fc9f0@alexandria-db.internal:5432/postgres`, 3, true);
+
+const supabaseUrl = "https://qksyrtriwpguwrkxyekd.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDc3NjQ4NSwiZXhwIjoxOTU2MzUyNDg1fQ.anrvkkFNQwp_Fao1DvQnaD56yJ1ucFNrAt3Q4Iw3jqQ";
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  fetch,
+});
+
+/**
+ */
+interface OS {
+  /**
+   * @returns {any[]}
+   */
+  get_items(): Promise<any>[];
+  /**
+   * @param {string} name
+   * @returns {string}
+   */
+  add_type(name: string): Promise<string>;
+  /**
+   * @returns {any[]}
+   */
+  get_types(): Promise<any>[];
+  /**
+   * @param {string} id
+   * @returns {any[]}
+   */
+  get_item_types(id: string): Promise<any>[];
+  /**
+   * @param {string} t
+   * @returns {string}
+   */
+  add_item(t: string): Promise<string>;
+  /**
+   * @param {string} a
+   * @param {string} b
+   */
+  add_edge(a: string, b: string): Promise<void>;
+  /**
+   * @returns {BigInt}
+   */
+  get_vertex_count(): Promise<BigInt>;
+  /**
+   * @param {string} a
+   * @returns {BigInt}
+   */
+  get_edge_count(a: string): Promise<BigInt>;
+  /**
+   * @param {string} id
+   * @returns {any}
+   */
+  get_properties(id: string): Promise<any>;
+  /**
+   * @param {string} id
+   * @param {string} name
+   * @param {string} value
+   */
+  set_property(id: string, name: string, value: string): Promise<void>;
+  /**
+   * @param {string} a
+   * @returns {any[]}
+   */
+  get_references(a: string): Promise<string[]>[];
+  /**
+   * @param {string} a
+   * @returns {any[]}
+   */
+  get_referenced_by(a: string): Promise<string[]>[];
+}
+
+import { serve } from "https://deno.land/std@0.126.0/http/server.ts";
 import { makeExecutableSchema } from "https://deno.land/x/graphql_tools@0.0.2/mod.ts";
 import { gql } from "https://deno.land/x/graphql_tag@0.0.1/mod.ts";
 import renderPlayground from "https://esm.sh/@magiql/ide/render";
-import {
-  printSchema,
-  graphql,
-} from "https://deno.land/x/graphql_deno@v15.0.0/mod.ts";
+import { graphql } from "https://deno.land/x/graphql_deno@v15.0.0/mod.ts";
 import pluralize from "https://esm.sh/pluralize";
 
-import init, { OS } from "./os/indradb.js";
+// import type  { OS } from "./os/indradb.js";
 
-await init();
-export let os = OS.new();
+// await init();
+export let os: OS = {
+  add_type: async (name: string) => {},
+  add_item: async (name: string) => {},
+  set_property: () => {},
+  add_edge: () => {},
+  get_items: async () => {
+    return (await supabase.from("items").select("id")).data;
+  },
+  get_references: async (id) => {
+    const { data } = await supabase
+      .from("references")
+      .select("*")
+      .eq("outbound_id", id);
+    return data.map((d) => d.inbound_id);
+  },
+  get_referenced_by: async (id) => {
+    const { data } = await supabase
+      .from("references")
+      .select("*")
+      .eq("inbound_id", id);
+    return data.map((d) => d.outbound_id);
+  },
+  get_item_types: async (id) => {
+    console.log({ id });
+    const { data } = await supabase
+      .from("references")
+      .select("*")
+      .eq("inbound_id", id);
+    console.log({ data });
+    if (!data.length) {
+      return ["Type"];
+    }
+    const { data: types } = await supabase
+      .from("items")
+      .select("*,item_properties(*)")
+      .in(
+        "id",
+        data.map((d) => d.outbound_id)
+      )
+      .eq("type", "Type");
+    let system = types.map(
+      (t) => t.item_properties.find((prop) => prop.name === "name").value
+    );
+
+    if (system.length === 1 && system[0] === "System") {
+      return ["Type"];
+    }
+    console.log({ system });
+    return system;
+  },
+  get_properties: async (id) => {
+    console.log("get_properties", id);
+    const {
+      data: [item],
+      errors,
+    } = await supabase
+      .from("items")
+      .select("*,item_properties(*)")
+      .eq("id", id);
+
+    console.log({ errors });
+    console.log({ item });
+    if (!item) {
+      return {};
+    }
+    console.log(
+      Object.fromEntries(item.item_properties.map((pr) => [pr.name, pr.value]))
+    );
+    return Object.fromEntries(
+      item.item_properties.map((pr) => [pr.name, pr.value])
+    );
+  },
+};
 
 const builder = (os: OS) => {
   let itemInterface = `
@@ -35,30 +175,34 @@ const builder = (os: OS) => {
 
   let userTypeDefs = ``;
 
-  function getReferences(id: any, args: { type: string }) {
-    const references = os
-      .get_references(id)
-      .filter((id) =>
-        args?.type ? os.get_item_types(id).includes(args.type) : true
-      )
-      .map((id: any) => ({
-        cursor: id,
-      }));
+  async function getReferences(id: any, args: { type: string }) {
+    let references = [];
+    for (let ref of await os.get_references(id)) {
+      if (
+        args?.type ? (await os.get_item_types(id)).includes(args.type) : true
+      ) {
+        references.push({
+          cursor: ref,
+        });
+      }
+    }
 
     return {
       page: references,
     };
   }
 
-  function getReferencenedBy(id: any, args: { type: string }) {
-    const references = os
-      .get_referenced_by(id)
-      .filter((id) =>
-        args?.type ? os.get_item_types(id).includes(args.type) : true
-      )
-      .map((id: any) => ({
-        cursor: id,
-      }));
+  async function getReferencenedBy(id: any, args: { type: string }) {
+    let references = [];
+    for (let ref of await os.get_referenced_by(id)) {
+      if (
+        args?.type ? (await os.get_item_types(id)).includes(args.type) : true
+      ) {
+        references.push({
+          cursor: ref,
+        });
+      }
+    }
 
     return {
       page: references,
@@ -121,27 +265,41 @@ const builder = (os: OS) => {
             )
             .join("\n")}
         }
+
+        type Mutation {
+          addType(name: String): Type
+        }
       `;
       const schema = makeExecutableSchema({
         typeDefs: gql(typeDefs),
         resolvers: {
+          Mutation: {
+            addType: async (root: any, args: any) => {
+              let id = os.add_type(args.name);
+              return {
+                id,
+                ...(await os.get_properties(id)),
+              };
+            },
+          },
           Query: {
             item: (root: any, args: any) => {
               // return os.get_item(args.id);
             },
-            items: (root: any, args: any) => {
-              return {
-                page: os
-                  .get_items()
-                  .filter((item: any) =>
-                    args.type
-                      ? os.get_item_types(item).includes(args.type)
-                      : true
-                  )
-                  .map((item: any) => ({
-                    cursor: item,
-                  })),
-              };
+            items: async (root: any, args: any) => {
+              let items = await os.get_items();
+
+              let page = [];
+              for (let item of items) {
+                if (
+                  args.type
+                    ? (await os.get_item_types(item.id)).includes(args.type)
+                    : true
+                ) {
+                  page.push({ cursor: item.id });
+                }
+              }
+              return { page };
             },
 
             ...Object.fromEntries(
@@ -173,10 +331,14 @@ const builder = (os: OS) => {
             types: (root: any) => os.get_item_types(root.id),
           },
           Reference: {
-            item: (root: any, args: any) => {
+            item: async (root: any, args: any) => {
+              console.log({
+                id: root.cursor,
+                ...(await os.get_properties(root.cursor)),
+              });
               return {
                 id: root.cursor,
-                ...os.get_properties(root.cursor),
+                ...(await os.get_properties(root.cursor)),
               };
             },
           },
@@ -190,8 +352,9 @@ const builder = (os: OS) => {
                   getReferencenedBy(root.id, args),
                 ...resolvers,
                 types: (root: any) => os.get_item_types(root.id),
-                __isTypeOf: (root: any) =>
-                  os.get_item_types(root.id).includes(type),
+                __isTypeOf: async (root: any) => {
+                  return (await os.get_item_types(root.id)).includes(type);
+                },
               },
             ])
           ),
@@ -330,7 +493,6 @@ let handler = async (req: Request) => {
 
     return res;
   }
-  console.log(req.url);
   const res: any =
     req.method === "GET"
       ? new Response(await renderPlayground({ uri: "/" }), {
@@ -344,24 +506,6 @@ let handler = async (req: Request) => {
   return res;
 };
 
-listenAndServe(
-  {
-    port: 9999,
-  },
-  handler
-);
-
-// import {
-//   app,
-//   get,
-//   post,
-//   redirect,
-//   contentType,
-// } from "https://denopkg.com/syumai/dinatra/mod.ts";
-
-// const greeting = "<h1>Hello From Deno on Fly!</h1>";
-
-// app(
-//   get("/", () => greeting),
-//   get("/:id", ({ params }) => greeting + `</br>and hello to ${params.id}`)
-// );
+serve(handler, {
+  port: 9999,
+});
